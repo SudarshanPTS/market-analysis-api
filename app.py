c8b7c4c8c2a2d0d8b2a84b4b95a37711213f104c
SudarshanPTS <04.08.sudarshanpts@gmail.com>
2025-08-21 10:43:24 +0530

    preparing for deployment

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ from routes.company_info import company_info_bp
 from routes.stock_data import stock_data_bp
 from routes.historical_data import historical_data_bp
 from routes.analytics import analytics_bp
+import os
 
 app = Flask(__name__)
 
@@ -13,4 +14,5 @@ app.register_blueprint(historical_data_bp,url_prefix='/historical-data')
 app.register_blueprint(analytics_bp,url_prefix='/analytics')
 
 if __name__=='__main__':
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
